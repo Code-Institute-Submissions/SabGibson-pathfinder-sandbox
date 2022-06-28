a8c5f297c9b83118c8f84597312454cc4cc1250a
SabGibson <sabelogibson1@gmail.com>
2022-06-28 19:25:32 +0100

    function prop created and grid mouse event logged on console

--- src/components/Node/Node.js
+++ src/components/Node/Node.js
@@ -1,5 +1,6 @@
 import "./Node.css";
 const Node = (props) => {
+  // Decant parameters from prop variable
   const params = props.node;
   const isStart = params.isStart;
   const isEnd = params.isEnd;
@@ -8,13 +9,23 @@ const Node = (props) => {
   const xVal = params.xVal;
   const yVal = params.yVal;
 
+  const functionTester = props.functionTester;
+
+  // Define classes for node classification
   const terminalClass = isStart ? "start" : isEnd ? "end" : "";
   const wallClass = isWall ? "wall" : "";
   const visitedClass = isVisited ? "visited" : "";
+
+  // Define node methods for interaction with grid
+  const testFunc = () => {
+    functionTester(params);
+  };
+  // Return Jsx node component
   return (
     <div
       className={`node ${terminalClass} ${wallClass} ${visitedClass}`}
       id={`id-${xVal}-${yVal}`}
+      onClick={testFunc}
     ></div>
   );
 };
--- src/components/Pathfinder.js
+++ src/components/Pathfinder.js
@@ -1,10 +1,13 @@
+// import modules and components
 import { useEffect, useState } from "react";
 import "./Pathfinder.css";
 import Node from "./Node/Node";
 
+// variables that define grid size
 const rows = 20;
 const cols = 50;
 
+// node object constructor
 function newNode(col, row) {
   this.xVal = col;
   this.yVal = row;
@@ -14,7 +17,9 @@ function newNode(col, row) {
   this.isVisited = false;
 }
 
+// Pathfinder state functionless component that will be main interface for app
 const Pathfinder = () => {
+  // gridInit() function creates 2D array and populates with nodes
   const gridInit = () => {
     const grid = [];
     for (let i = 0; i < cols; i++) {
@@ -24,23 +29,39 @@ const Pathfinder = () => {
       }
       grid.push(currentRow);
     }
+    setMouseIsPressed(false);
     setActiveGrid(grid);
   };
 
+  // Define React state hooks for grid and mouse input
   const [activeGrid, setActiveGrid] = useState([]);
-  //   const [mouseIsPressed, setMouseIsPressed] = useState(false);
+  const [mouseIsPressed, setMouseIsPressed] = useState([]);
+
+  // Define effect hook of init of Pathfinder (gird) component
   useEffect(() => {
     gridInit();
   }, []);
 
+  // Pathfinder methods for interaction with grid
+  const functionTester = (node) => {
+    console.log(`ID${node.xVal}-${node.yVal}`);
+  };
+
+  // Define return variable for the Pathfinder using Node component
   const functionalGrid = (
     <div className="sb-grid">
       {activeGrid.map((row, rowIndex) => {
         return (
           <div key={rowIndex}>
             {row.map((node, nodeIndex) => {
-              console.log(node);
-              return <Node key={nodeIndex} node={node} />;
+              return (
+                <Node
+                  key={nodeIndex}
+                  node={node}
+                  mouseIsPressed={mouseIsPressed}
+                  functionTester={functionTester}
+                />
+              );
             })}
           </div>
         );
@@ -48,6 +69,7 @@ const Pathfinder = () => {
     </div>
   );
 
+  // Return jsx to  component in app interface
   return <div className="sandbox">{functionalGrid}</div>;
 };
 
